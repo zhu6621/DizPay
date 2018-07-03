5d13f29b86eacc723d5e47ab77e62b9ad8983298
AnswerYas <answeryas@gamil.com>
2018-07-03 18:54:44 +0800

    fix

--- demos/python/run.py
+++ demos/python/run.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
-from flask import session, request, redirect, jsonify
 from app import create_app
 from app.api import init_api
 
 app = create_app()
 init_api(app)
-
-
-def is_ajax_request():
-    return request.headers.get('X-Requested-With') == 'XMLHttpRequest'
 
 
 if __name__ == '__main__':
